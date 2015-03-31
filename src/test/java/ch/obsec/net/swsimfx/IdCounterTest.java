ac4bcb1af4ad2e90dac0a03f877cc3e0f9517188
Mario Oberli <mario.oberli@obsec.ch>
2015-04-01 00:59:55 +0200

    SSF-18 junit

---
+++ src/test/java/ch/obsec/net/swsimfx/IdCounterTest.java
@@ -0,0 +1,16 @@
+package ch.obsec.net.swsimfx;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by obm on 01.04.2015.
+ */
+public class IdCounterTest {
+
+    @Test
+    public void testNextId() throws Exception {
+
+    }
+}
---
+++ src/test/java/ch/obsec/net/swsimfx/model/DeviceTest.java
@@ -0,0 +1,14 @@
+package ch.obsec.net.swsimfx.model;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class DeviceTest {
+
+    @Test
+    public void testGetID() throws Exception {
+
+    }
+
+}
