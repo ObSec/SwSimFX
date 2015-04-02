f892517d6625777e292775f2e75eab1d78d739d2
Mario Oberli <mario.oberli@obsec.ch>
2015-04-02 21:31:57 +0200

    SSF-18 junit tests

--- src/test/java/ch/obsec/net/swsimfx/IdCounterTest.java
+++ src/test/java/ch/obsec/net/swsimfx/IdCounterTest.java
@@ -4,13 +4,10 @@ import org.junit.Test;
 
 import static org.junit.Assert.*;
 
-/**
- * Created by obm on 01.04.2015.
- */
 public class IdCounterTest {
 
     @Test
     public void testNextId() throws Exception {
-
+        IdCounter.nextId();
     }
 }
